b6e9a0fb1e5dacc2d1ced5d49a4157a07bba2747
somnath khadanga <somnathkhadanga@192.168.1.176>
2023-06-03 13:20:33 +0530

    blog strucutre

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+import "@/styles/globals.scss";
+import { nunito } from "../../Util/Font";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main>
+      className={nunito.className}
+      <Component {...pageProps} />
+    </main>
+  );
+}
